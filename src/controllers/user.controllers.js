92ecca03797fee2570eb45390ac4872b7b7b52b7
Ricardo Velez <ricardovelecal@gmail.com>
2023-02-11 12:18:20 -0600

    user.controllers.js with db.js: connection

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -1,11 +1,35 @@
+import { pool } from "../db.js";
+
+const dbInaccessible =  'Something goes wrong';
+
 export const getUsers = async (req, res) => {
-    return res.json({
-        message: 'getting users'
-    });
+    try {
+        const [rows] = await pool.query(
+            `SELECT * FROM user`
+        );
+        res.json(rows)
+    } catch (err) {
+        return res.status(500).json({
+            message: dbInaccessible
+        });
+    }
 }
 
 export const createUser = async (req, res) => {
-    return res.json({
-        message: 'creating users'
-    });
+    try {
+        const {name, email} = req.body;
+        const [rows] = await pool.query(
+            `INSERT INTO user (name, email, creation_date) VALUES (?, ?, CURDATE())`,
+            [name, email]
+        );
+        res.send({
+            id: rows.insertId,
+            name,
+            email, 
+        });
+    } catch (err) {
+        return res.status(500).json({
+            message: dbInaccessible
+        });
+    }
 }
