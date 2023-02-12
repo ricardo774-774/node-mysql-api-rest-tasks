a6a74a7a7d1619461870aba0c289f0266a6d1a0d
Ricardo Velez <ricardovelecal@gmail.com>
2023-02-11 18:46:14 -0600

    task.controllers.js with db.js: connection

--- src/controllers/task.controllers.js
+++ src/controllers/task.controllers.js
@@ -1,23 +1,147 @@
+import { pool } from "../db.js";
+
+const dbInaccessible =  'Something goes wrong';
+
 export const getTasks = async (req, res) => {
-    return res.json({
-        message: 'getting tasks'
-    });
+    try {
+        const [rows] = await pool.query(
+            `SELECT 
+                title, 
+                description, 
+                isCompleted, 
+                deadline
+            FROM task`
+        );
+        res.json(rows)
+    } catch (err) {
+        return res.status(500).json({
+            message: dbInaccessible
+        });
+    }
+}
+
+export const getTask = async (req, res) => {
+    try {
+        const {id} = req.params;
+        const [rows] = await pool.query(
+            `SELECT * FROM task WHERE id = ?`, 
+            [id]
+        );
+        res.send(rows[0]);
+    } catch (err) {
+        return res.status(500).json({
+            message: dbInaccessible
+        });
+    }
 }
 
 export const createTask = async (req, res) => {
-    return res.json({
-        message: 'creating tasks'
-    });
+    try {
+        const { 
+            title, 
+            description, 
+            isCompleted, 
+            responsible,
+            deadline, 
+            comments, 
+            id_user, 
+            tags 
+        } = req.body;
+
+        const [rows] = await pool.query(
+            `INSERT INTO task ( 
+                title, 
+                description, 
+                isCompleted, 
+                deadline, 
+                id_user,
+                comments, 
+                responsible, 
+                tags
+            ) VALUES (?, ?, ?, ?, ?, ?, ?, ?)`,
+            [ 
+                title, 
+                description, 
+                isCompleted, 
+                deadline,
+                id_user,
+                comments,
+                responsible,
+                tags 
+            ]
+        );
+        res.send({
+            id: rows.insertId,
+            title,
+            description,
+            isCompleted,
+            deadline,
+            comments,
+            id_user, 
+            responsible,
+            tags
+        });
+    } catch (err) {
+        console.log(err);
+        return res.status(500).json({
+            message: dbInaccessible
+        });
+    }
 }
 
 export const updateTask = async(req, res) => {
-    return res.json({
-        message: 'updating tasks'
-    });
+    try {
+        const {id} = req.params;
+        const { 
+            title, 
+            description, 
+            isCompleted, 
+            responsible,
+            deadline, 
+            comments, 
+            id_user, 
+            tags 
+        } = req.body;
+        const [result] = await pool.query(
+            `UPDATE task SET 
+                title = IFNULL(?, title), 
+                description = IFNULL(?, description),
+                isCompleted = IFNULL(?, isCompleted),
+                deadline = IFNULL(?, deadline),
+                id_user = IFNULL(?, id_user),
+                comments = IFNULL(?, comments),
+                responsible = IFNULL(?, responsible),
+                tags = IFNULL(?, tags)
+            WHERE id = (?)`,
+            [ 
+                title, 
+                description, 
+                isCompleted, 
+                deadline,
+                id_user,
+                comments,
+                responsible,
+                tags,
+                id
+            ]
+        );
+    } catch (err) {
+        return res.status(500).json({
+            message: dbInaccessible
+        });
+    }
 }
 
 export const deleteTask = async (req, res) => {
-    return res.json({
-        message: 'removing tasks'
-    });
+    try {
+        const {id} = req.params;
+        const [result] = await pool.query(
+            `DELETE FROM task WHERE id = ?`,
+            [id]
+        );
+    } catch (error) {
+        return res.status(500).json({
+            message: dbInaccessible
+        });
+    }
 }
--- src/routes/task.routes.js
+++ src/routes/task.routes.js
@@ -3,17 +3,20 @@ import {
     getTasks,
     createTask,
     updateTask,
-    deleteTask
+    deleteTask,
+    getTask
 } from "../controllers/task.controllers.js";
 
 const router =  Router();
 
 router.get('/tasks', getTasks);
 
+router.get('/tasks/:id', getTask);
+
 router.post('/tasks', createTask);
 
-router.patch('/tasks', updateTask);
+router.patch('/tasks/:id', updateTask);
 
-router.delete('/tasks', deleteTask);
+router.delete('/tasks/:id', deleteTask);
 
 export default router;
