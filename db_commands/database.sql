b371da10af6f4982dca40d1bb6325f9c23b606f8
Ricardo Velez <ricardovelecal@gmail.com>
2023-02-11 18:58:15 -0600

    db_commands: updated

--- db_commands/database.sql
+++ db_commands/database.sql
@@ -13,11 +13,12 @@ create table user (
 create table task (
 	id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
     title VARCHAR(100) NOT NULL,
-    description INT NOT NULL,
+    description VARCHAR(200) NOT NULL,
     isCompleted BOOL NOT NULL,
     deadline DATE NOT NULL,
-    comments VARCHAR(400) DEFAULT NULL,
-    id_user INT,
+    id_user INT NOT NULL,
     CONSTRAINT FK_TaskUser FOREIGN KEY(id_user) REFERENCES user(id) ON UPDATE CASCADE,
-    tags ENUM('job', 'school', 'family', 'friends', 'couple') DEFAULT NULL
+    comments VARCHAR(400) DEFAULT NULL,
+    responsible VARCHAR(100) DEFAULT NULL,
+    tags ENUM('job', 'school', 'family', 'friends', 'couple', 'home') DEFAULT NULL
 );
